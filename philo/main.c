22124cb1ba4f9d34b940fca800fbfdb7321475e0
ribana-b <ribana-b@student.42malaga.com>
2024-07-04 11:35:11 +0200

    Save for reset

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: ribana-b <ribana-b@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 00:05:11 by ribana-b          #+#    #+# Malaga      */
-/*   Updated: 2024/07/01 11:45:16 by ribana-b         ###   ########.com      */
+/*   Updated: 2024/07/04 10:32:20 by ribana-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,59 +39,54 @@ static int	parse_arguments(t_info *info)
 
 void	take_forks(t_philo *philo)
 {
-	int	index[2];
-
 	pthread_mutex_lock(&philo->info->mutex);
-	index[LEFT] = philo->id % philo->info->n_philo;
-	index[RIGHT] = (philo->id + 1) % philo->info->n_philo;
-	pthread_mutex_unlock(&philo->info->mutex);
-	if (index[LEFT] < index[RIGHT])
+	if (philo->id % 2 == 0)
 	{
-		pthread_mutex_lock(&philo->info->table.forks[index[LEFT]]);
+		pthread_mutex_lock(&philo->info->table.forks[philo->forks[RIGHT]]);
 		print_message(philo, TAKING_FORK);
-		pthread_mutex_lock(&philo->info->table.forks[index[RIGHT]]);
+		pthread_mutex_lock(&philo->info->table.forks[philo->forks[LEFT]]);
 		print_message(philo, TAKING_FORK);
 	}
 	else
 	{
-		pthread_mutex_lock(&philo->info->table.forks[index[RIGHT]]);
+		pthread_mutex_lock(&philo->info->table.forks[philo->forks[LEFT]]);
 		print_message(philo, TAKING_FORK);
-		pthread_mutex_lock(&philo->info->table.forks[index[LEFT]]);
+		pthread_mutex_lock(&philo->info->table.forks[philo->forks[RIGHT]]);
 		print_message(philo, TAKING_FORK);
 	}
+	pthread_mutex_unlock(&philo->info->mutex);
 	philo->status = TAKING_FORK;
 }
 
 void	start_eating(t_philo *philo)
 {
-	philo->time_of[EAT] = get_elapsed_time();
+	philo->time_of_eat = get_elapsed_time();
 	philo->status = EAT;
 	print_message(philo, EAT);
 	my_sleep(philo->info->limit_time_to[EAT]);
 	if (philo->info->n_meals != -1)
+	{
 		++philo->meal_counter;
-}
-
-void	leave_forks(t_philo *philo)
-{
-	int	index[2];
-
-	pthread_mutex_lock(&philo->info->mutex);
-	index[LEFT] = philo->id % philo->info->n_philo;
-	index[RIGHT] = (philo->id + 1) % philo->info->n_philo;
-	pthread_mutex_unlock(&philo->info->mutex);
-	pthread_mutex_unlock(&philo->info->table.forks[index[RIGHT]]);
-	pthread_mutex_unlock(&philo->info->table.forks[index[LEFT]]);
+	}
+	if (philo->id % 2 == 0)
+	{
+		pthread_mutex_unlock(&philo->info->table.forks[philo->forks[LEFT]]);
+		pthread_mutex_unlock(&philo->info->table.forks[philo->forks[RIGHT]]);
+	}
+	else
+	{
+		pthread_mutex_unlock(&philo->info->table.forks[philo->forks[RIGHT]]);
+		pthread_mutex_unlock(&philo->info->table.forks[philo->forks[LEFT]]);
+	}
+	philo->last_meal = get_elapsed_time();
 }
 
 void	start_sleeping(t_philo *philo)
 {
-	philo->time_of[SLEEP] = get_elapsed_time();
+	philo->time_of_sleep = get_elapsed_time();
 	philo->status = SLEEP;
 	print_message(philo, SLEEP);
-	pthread_mutex_lock(&philo->info->mutex);
 	my_sleep(philo->info->limit_time_to[SLEEP]);
-	pthread_mutex_unlock(&philo->info->mutex);
 }
 
 void	start_thinking(t_philo *philo)
@@ -102,43 +97,48 @@ void	start_thinking(t_philo *philo)
 	print_message(philo, THINK);
 }
 
-bool	finished_meals(t_info *info)
-{
-	int		index;
-	bool	is_finished;
-
-	index = 0;
-	is_finished = true;
-	while (index < info->n_philo && is_finished)
-	{
-		if (info->table.philo[index].meal_counter != info->n_meals)
-			is_finished = false;
-		++index;
-	}
-	return (is_finished);
-}
-
 void	*routine(void *data)
 {
 	t_philo		*philo;
 
 	philo = data;
+	if (philo->id % 2 != 0)
+		usleep(60 * philo->info->n_philo);
 	while (1)
 	{
 		take_forks(philo);
 		start_eating(philo);
-		leave_forks(philo);
+		pthread_mutex_lock(&philo->info->mutex);
+		if (philo->meal_counter == philo->info->n_meals)
+		{
+			pthread_mutex_unlock(&philo->info->mutex);
+			return (NULL);
+		}
+		pthread_mutex_unlock(&philo->info->mutex);
 		start_sleeping(philo);
 		start_thinking(philo);
-		if (philo->meal_counter == philo->info->n_meals)
-			break ;
 	}
 	return (NULL);
 }
 
-int	create_simulation_threads(t_info *info)
+void	*checker(void *data)
 {
-	int	index;
+	t_info	*info;
+
+	info = data;
+	while (1)
+	{
+		pthread_mutex_lock(&info->mutex);
+		pthread_mutex_unlock(&info->mutex);
+		usleep(100);
+	}
+	return (NULL);
+}
+
+int	start_simulation(t_info *info)
+{
+	t_pthread	checker_thread;
+	int			index;
 
 	index = 0;
 	while (index < info->n_philo)
@@ -148,57 +148,15 @@ int	create_simulation_threads(t_info *info)
 			return (error_handler(info, KO));
 		++index;
 	}
-	return (OK);
-}
-
-void	join_simulation_threads(t_info *info)
-{
-	int	index;
-
+	if (pthread_create(&checker_thread, NULL, &checker, info))
+		return (KO);
 	index = 0;
 	while (index < info->n_philo)
 	{
 		pthread_join(info->table.philo[index].thread, NULL);
 		++index;
 	}
-}
-
-void	*checker(void *data)
-{
-	t_info	*info;
-	int		index;
-	bool	finished;
-
-	info = data;
-	index = 0;
-	finished = true;
-	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nNICE\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-	pthread_mutex_lock(&info->mutex);
-	while (index < info->n_philo && finished)
-	{
-		if (info->table.philo[index].meal_counter != info->n_meals)
-			finished = false;
-		++index;
-	}
-	pthread_mutex_unlock(&info->mutex);
-	if (!finished)
-		return (NULL);
-	info->finish = true;
-	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nNICE\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-	return (info);
-}
-
-int	start_simulation(t_info *info)
-{
-	//t_pthread	thread;
-
-	info->finish = false;
-	if (create_simulation_threads(info) != OK)
-		return (KO);
-	join_simulation_threads(info);
-	//if (pthread_create(&thread, NULL, &checker, info) != OK)
-	//	return (KO);
-	//pthread_join(thread, NULL);
+	pthread_join(checker_thread, NULL);
 	return (OK);
 }
 
@@ -210,9 +168,11 @@ int	finish_simulation(t_info *info)
 	while (++index < info->n_philo && info->error.status != RIP_MALLOC)
 	{
 		pthread_mutex_destroy(&info->table.forks[index]);
+		pthread_mutex_destroy(&info->table.philo[index].mutex);
 	}
 	pthread_mutex_destroy(&info->mutex);
 	pthread_mutex_destroy(&info->print_mutex);
+	pthread_mutex_destroy(&info->table.mutex);
 	free(info->table.forks);
 	free(info->table.philo);
 	return (info->error.status);
